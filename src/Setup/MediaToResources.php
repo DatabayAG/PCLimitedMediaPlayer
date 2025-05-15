99566a14ed015533602ee06fea666bedf6b7945f
fneumann <fneumann@databay.de>
2025-05-15 17:08:00 +0200

    Fault tolerant media migration

--- src/Setup/MediaToResources.php
+++ src/Setup/MediaToResources.php
@@ -17,13 +17,16 @@ use ilPCLimitedMediaPlayerPlugin;
 use ILIAS\Plugin\LimitedMediaPlayer\MediumRepo;
 use Exception;
 use ILIAS\Filesystem\Filesystem;
+use ilLogger;
+use ilObject;
 
 class MediaToResources
 {
     private ilDBInterface $db;
+    private MediumRepo $medium_repo;
     private Filesystem $fs;
     private ilPCLimitedMediaPlayerPlugin $plugin;
-    private MediumRepo $medium_repo;
+    private ilLogger $logger;
 
     public function __construct()
     {
@@ -35,73 +38,83 @@ class MediaToResources
 
         $this->db = $DIC->database();
         $this->fs = $DIC->filesystem()->web();
+        $this->logger = $DIC->logger()->limply();
         $this->plugin = $DIC["component.factory"]->getPlugin(ilPCLimitedMediaPlayerPlugin::ID);
         $this->medium_repo = $this->plugin->factory()->mediumRepo();
     }
 
     public function execute(): void
     {
-        $query = "SELECT page_id, content FROM page_object "
-            . " WHERE " . $this->db->like('content', 'text', '%PCLimitedMediaPlayer%', false);
+        $query = "
+            SELECT page_id, parent_id, content FROM page_object
+            WHERE content LIKE '%PCLimitedMediaPlayer%';
+        ";
         $result = $this->db->query($query);
 
         while ($row = $this->db->fetchAssoc($result)) {
-            $dom = new DOMDocument("1.0", "UTF-8");
-            $dom->loadXML($row['content']);
-            $xpath = new DOMXPath($dom);
-            $player_nodes = $xpath->query("//Plugged[@PluginName='PCLimitedMediaPlayer']/parent::PageContent");
+            try {
+                $dom = new DOMDocument("1.0", "UTF-8");
+                $dom->loadXML($row['content']);
+                $xpath = new DOMXPath($dom);
+                $player_nodes = $xpath->query("//Plugged[@PluginName='PCLimitedMediaPlayer']/parent::PageContent");
 
-            if (!empty($player_nodes)) {
-                $page = new ilAssQuestionPage((int) $row['page_id']);
-                $page->buildDom();
+                if (!empty($player_nodes)) {
+                    $page = new ilAssQuestionPage((int) $row['page_id']);
+                    $page->buildDom();
 
-                /** @var DOMElement $player_node */
-                foreach ($player_nodes as $player_node) {
-                    $file_name = '';
-                    $preview_name = '';
+                    /** @var DOMElement $player_node */
+                    foreach ($player_nodes as $player_node) {
+                        $file_name = '';
+                        $preview_name = '';
 
-                    /** @var ilPCPlugged $player_content */
-                    $player_pcid = $player_node->getAttribute('PCID');
-                    $player_content = $page->getContentObjectForPcId($player_pcid);
-                    if ($player_content !== null) {
-                        $properties = $player_content->getProperties();
+                        /** @var ilPCPlugged $player_content */
+                        $player_pcid = $player_node->getAttribute('PCID');
+                        $player_content = $page->getContentObjectForPcId($player_pcid);
+                        if ($player_content !== null) {
+                            $properties = $player_content->getProperties();
 
-                        $medium_pcid = $properties['medium_pcid'] ?? '';
-                        /** @var ilPCMediaObject $medium_content */
-                        if ($medium_pcid !== '') {
-                            $medium_content = $page->getContentObjectForPcId($medium_pcid);
-                            if ($medium_content !== null) {
-                                $media_object = $medium_content->getMediaObject();
-                                if ($media_object !== null) {
-                                    /** @var ilMediaItem $item */
-                                    $item = $media_object->getMediaItem('Fullscreen');
-                                    if ($item !== null) {
-                                        $file = $item->getLocation();
-                                        $preview = $media_object->getVideoPreviewPic(true);
+                            $medium_pcid = $properties['medium_pcid'] ?? '';
+                            /** @var ilPCMediaObject $medium_content */
+                            if ($medium_pcid !== '') {
+                                $medium_content = $page->getContentObjectForPcId($medium_pcid);
+                                if ($medium_content !== null) {
+                                    $media_object = $medium_content->getMediaObject();
+                                    if ($media_object !== null) {
+                                        /** @var ilMediaItem $item */
+                                        $item = $media_object->getMediaItem('Fullscreen');
+                                        if ($item !== null) {
+                                            $file = $item->getLocation();
+                                            $preview = $media_object->getVideoPreviewPic(true);
 
-                                        $file_id = $this->migrateMobFile($media_object->getId(), $file);
-                                        $preview_id = $this->migrateMobFile($media_object->getId(), $preview);
+                                            $file_id = $this->migrateMobFile($media_object->getId(), $file);
+                                            $preview_id = $this->migrateMobFile($media_object->getId(), $preview);
 
-                                        $page->deleteContent('', false, $medium_content->getPCId());
-                                        $player_content->setProperties([
-                                            'file_id' => $file_id,
-                                            'preview_id' => $preview_id,
-                                            'title' => $properties['medium_title'] ?? '',
-                                            'limit_plays' => $properties['limit_plays'] ?? '',
-                                            'limit_context' => $properties['limit_context'] ?? '',
-                                            'width' => $properties['medium_width'] ?? '',
-                                            'height' => $properties['medium_height'] ?? '',
-                                            'play_in_modal' => $properties['play_modal'] ?? '',
-                                            'play_with_pause' => $properties['play_pause'] ?? ''
-                                        ]);
+                                            $page->deleteContent('', false, $medium_content->getPCId());
+                                            $player_content->setProperties([
+                                                'file_id' => $file_id,
+                                                'preview_id' => $preview_id,
+                                                'title' => $properties['medium_title'] ?? '',
+                                                'limit_plays' => $properties['limit_plays'] ?? '',
+                                                'limit_context' => $properties['limit_context'] ?? '',
+                                                'width' => $properties['medium_width'] ?? '',
+                                                'height' => $properties['medium_height'] ?? '',
+                                                'play_in_modal' => $properties['play_modal'] ?? '',
+                                                'play_with_pause' => $properties['play_pause'] ?? ''
+                                            ]);
 
-                                        $page->update();
-                                        $media_object->delete();
+                                            $page->update();
+                                            $media_object->delete();
+                                        }
                                     }
                                 }
                             }
                         }
                     }
+                }
+            } catch (\Throwable $e) {
+                foreach (ilObject::_getAllReferences((int) $row['parent_id'] ?? 0) as $ref_id) {
+                    $this->logger->error($e->getMessage() . "  (pool_ref_id: {$ref_id}, question_id: {$row['page_id']}')");
+                    break;
                 }
             }
         }
