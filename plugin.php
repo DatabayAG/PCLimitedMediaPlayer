6bddb32cf854dfc3cdbc464d85854d073ac2db0d
fneumann <fneumann@databay.de>
2025-12-11 08:22:56 +0100

    Version 4.0.0

--- plugin.php
+++ plugin.php
@@ -10,7 +10,7 @@
 $id = "limply";
 
 $version = "4.0.0";
-$ilias_min_version = "10.2";
+$ilias_min_version = "10.3";
 $ilias_max_version = "10.999";
 $responsible = "Fred Neumann";
 $responsible_mail = "fneumann@databay.de";
